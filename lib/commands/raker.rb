0e9f58f75b592ad6d38aca5632de5cd9c5535fcb
David Heinemeier Hansson <david@loudthinking.com>
2012-12-22 16:23:34 +0100

    Allow you to set Rake ENV options as part of the call (still missing a way to set rake options like --trace)

--- lib/commands/raker.rb
+++ lib/commands/raker.rb
@@ -8,8 +8,8 @@ module Commands
       load_rake_tasks
     end
 
-    def rake(task = nil, *args)
-      task.nil? ? print_rake_tasks : invoke_rake_task(task, *args)
+    def rake(task = nil)
+      task.nil? ? print_rake_tasks : invoke_rake_task(task)
       "Completed"
     rescue SystemExit, RuntimeError => e
       "Failed: #{e.message}"
@@ -28,9 +28,33 @@ module Commands
         Rake.application.display_tasks_and_comments
       end
 
-      def invoke_rake_task(task, *args)
-        Rake::Task[task].invoke(*args)
+      def invoke_rake_task(task)
+        task, *options = task.split(" ")
+
+        ARGV.replace options
+
+        # FIXME: Before we can use this, we need a way to reset the options again
+        # Rake.application.handle_options
+
+        expose_argv_arguments_via_env { Rake::Task[task].invoke }
         Rake.application.tasks.each(&:reenable) # Rake by default only allows tasks to be run once per session
+      ensure
+        ARGV.replace([])
+      end
+      
+      def expose_argv_arguments_via_env
+        argv_arguments.each { |key, value| ENV[key] = value }
+        yield
+      ensure
+        argv_arguments.keys.each { |key| ENV.delete(key) }
+      end
+      
+      def argv_arguments
+        ARGV.each_with_object({}) do |arg, arguments|
+          if arg =~ /^(\w+)=(.*)$/
+            arguments[$1] = $2
+          end
+        end
       end
   end
 end
